19744503e465a5b235bae2175abeaa39a5f701dc
driyu <pedriyu@gmail.com>
2019-04-06 21:34:31 +0200

    Generado componente Card.vue y añadido a la app para visualizarlo

--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Card from './pages/home/components/Card.vue'
 import router from './router'
 import './main.css'
 
 Vue.config.productionTip = false
 
+Vue.component('card', Card)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: { App, Card },
   template: '<App/>'
 })
---
+++ src/pages/home/components/Card.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="col-md-4">
+    <div class="card mb-4 shadow-sm">
+      <svg
+        class="bd-placeholder-img card-img-top"
+        width="100%"
+        height="225"
+        xmlns="http://www.w3.org/2000/svg"
+        preserveAspectRatio="xMidYMid slice"
+        focusable="false"
+        role="img"
+        aria-label="Placeholder: Thumbnail"
+      >
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#55595c"></rect>
+        <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
+      </svg>
+      <div class="card-body">
+        <h4>{{heroName}}</h4>
+        <p class="card-text">{{ text }}</p>
+        <div class="d-flex justify-content-center align-items-center mx-auto">
+          <div class="btn-group">
+            <button type="button" class="btn btn-danger">View</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  data () {
+    return {
+      heroName: 'Nombre del héroe',
+      text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit vehicula nulla, curabitur sollicitudin eu vitae condimentum lectus consequat. Iaculis volutpat platea pulvinar suscipit habitasse sociosqu eleifend tristique aptent, ultrices at rutrum sociis potenti duis ante dignissim, leo quis ad eget fermentum nibh sodales egestas. Vulputate per tempus blandit mauris posuere scelerisque nam faucibus, hac erat torquent tristique luctus iaculis magnis, etiam pulvinar in augue lacinia pellentesque porta.',
+      photoThumbnail: ''
+    }
+  }
+}
+
+</script>
+
+<style>
+.hero {
+  font-size: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
