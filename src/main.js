d69e8e3afd7d9766c25841ad5d3526d83ac6ebf7
driyu <pedriyu@gmail.com>
2019-04-13 11:05:11 +0200

    Eliminado la plantilla original de Vue y empezando a trabajar con la plantilla Home del proyecto

--- src/main.js
+++ src/main.js
@@ -3,17 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import Card from './pages/home/components/Card.vue'
+import Home from './pages/home/containers/Home.vue'
 import router from './router'
 import './main.css'
 
 Vue.config.productionTip = false
 
 Vue.component('card', Card)
+Vue.component('home', Home)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App, Card },
+  components: { App, Home, Card },
   template: '<App/>'
 })
---
+++ src/pages/home/containers/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <p>Componente header</p>
+    <p>Componente SearchBar</p>
+    <div>Grid<card></card></div>
+    <p>footer</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      datos: 'Hola mundo!'
+    }
+  }
+}
+
+</script>
+
+<style>
+.hero {
+  font-size: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
