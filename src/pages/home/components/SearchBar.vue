28ea112fc9b668f858d5ed9d6ea5394c42309439
Gabriela <gabriela.grojas@gmail.com>
2019-04-13 12:15:16 +0200

    cambiar espacios de 4 a 2

--- src/pages/home/components/SearchBar.vue
+++ src/pages/home/components/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="form-group has-search">
-            <span class="fa fa-search form-control-feedback"></span>
-            <input type="text" class="form-control" placeholder="Search">
-          </div>
+  <div class="form-group has-search">
+    <span class="fa fa-search form-control-feedback"></span>
+      <input type="text" class="form-control" placeholder="Search">
+  </div>
 </template>
