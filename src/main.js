1a0e94cea6166c93482a12e0c3fab12a9cf842a5
Gabriela <gabriela.grojas@gmail.com>
2019-04-13 11:54:33 +0200

    agregar el SearchBar a la pagina principal

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import Card from './pages/home/components/Card.vue'
+import Card from './pages/home/components/Card'
+import SearchBar from './pages/home/components/SearchBar'
 import router from './router'
 import './main.css'
 
 Vue.config.productionTip = false
 
 Vue.component('card', Card)
+Vue.component('searchBar', SearchBar)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App, Card },
+  components: { App, Card, SearchBar },
   template: '<App/>'
 })
---
+++ src/pages/home/components/SearchBar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="form-group has-search">
+            <span class="fa fa-search form-control-feedback"></span>
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+</template>
