9d255f120096d444a423316c249d6a161bf1b498
Fabiola Vieyra <fabiolacvieyra@gmail.com>
2019-04-13 13:59:37 +0200

    Footer (#15)
    * Añade Footer. Close #4

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -4,15 +4,14 @@
       <p class="float-right">
         <a href="#" class="text-white"><i class="fas fa-arrow-up"></i> Volver arriba</a>
       </p>
-      <p class="text-footer">WikiMarvel Guild Ideas - Design: <a href="https://twitter.com/mrcodedev/" target="_blank" class="text-danger">@MrCodedev</a>
-      </p>
+      <p class="text-footer">WikiMarvel Guild Ideas - Design: <a href="https://twitter.com/mrcodedev/" target="_blank" class="text-danger">@MrCodedev</a></p>
       <p class="text-footer"><small>Los logos y material utilizados de Marvel para esta guild, son sólo de proposito educativo, no nos lucramos de ello y toda actividad que realizamos es totalmente gratuita. </small></p>
       <p class="text-footer"><small>Open Source Weekends - Guild Ideas 2019</small></p>
     </div>
   </footer>
 </template>
 <script>
-  export default {
-    name: 'footerBar',
-  }
+export default {
+  name: 'FooterBar'
+}
 </script>
--- src/pages/home/containers/Home.vue
+++ src/pages/home/containers/Home.vue
@@ -3,16 +3,7 @@
     <headerBar />
     <searchBar />
     <grid v-bind:cards="cards" />
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#" class="text-white"><i class="fas fa-arrow-up"></i> Volver arriba</a>
-        </p>
-        <p class="text-footer">WikiMarvel Guild Ideas - Design: <a href="https://twitter.com/mrcodedev/" target="_blank" class="text-danger">@MrCodedev</a></p>
-        <p class="text-footer"><small>Los logos y material utilizados de Marvel para esta guild, son sólo de proposito educativo, no nos lucramos de ello y toda actividad que realizamos es totalmente gratuita. </small></p>
-        <p class="text-footer"><small>Open Source Weekends - Guild Ideas 2019</small></p>
-      </div>
-    </footer>
+    <footerBar />
   </div>
 </template>
 
@@ -20,13 +11,15 @@
 import HeaderBar from '../../../components/HeaderBar'
 import SearchBar from '../components/SearchBar'
 import Grid from '../components/Grid'
+import FooterBar from '../../../components/FooterBar'
 
 export default {
   name: 'Home',
   components: {
     HeaderBar,
     SearchBar,
-    Grid
+    Grid,
+    FooterBar
   },
   data: () => {
     return {cards: [1, 2, 3, 4, 5, 6]}
